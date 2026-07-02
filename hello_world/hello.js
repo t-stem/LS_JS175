ee46a53077677d4f3b24bf6a6171076920ff2504
Tim <N/A>
2026-07-02 15:59:13 +0100

    js175 5.6 added French, Serbian and Japanese pages

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -8,9 +8,16 @@ APP.set("view engine", "pug");
 
 APP.use(EXPRESS.static('public'));
 
-APP.get("/", (request, response) => {
-  response.render('hello-world-english');
-});
+const renderEnglishView = (request, response) => response.render("hello-world-english");
+const renderFrenchView = (request, response) => response.render("hello-world-french");
+const renderSerbianView = (request, response) => response.render("hello-world-serbian");
+const renderJapaneseView = (request, response) => response.render("hello-world-japanese");
+
+APP.get("/", renderEnglishView);
+APP.get("/english", renderEnglishView);
+APP.get("/french", renderFrenchView);
+APP.get("/serbian", renderSerbianView);
+APP.get("/japanese", renderJapaneseView);
 
 let logPortNumber = () => console.log(`Listening on port: ${PORT}`);
 APP.listen(PORT, "localhost", logPortNumber);
