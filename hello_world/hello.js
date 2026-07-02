ddec8f7c845e9f267e39031b3f11cc03f293c28d
Tim <N/A>
2026-07-02 23:29:37 +0100

    js175 5.9 added view helper functions

--- hello_world/hello.js
+++ hello_world/hello.js
@@ -11,16 +11,32 @@ app.set("view engine", "pug");
 app.use(express.static('public'));
 app.use(morgan("common"));
 
-const englishVars = {englishClass: "current"};
-const frenchVars = {frenchClass: "current"};
-const japaneseVars = {japaneseClass: "current"};
-const serbianVars = {serbianClass: "current"};
-const renderEnglishView = (request, response) => response.render("hello-world-english", englishVars);
-const renderFrenchView = (request, response) => response.render("hello-world-french", frenchVars);
-const renderSerbianView = (request, response) => response.render("hello-world-serbian", serbianVars);
-const renderJapaneseView = (request, response) => response.render("hello-world-japanese", japaneseVars);
+app.locals.currentClassForRequestedPath = (navPath, requestedPath) => navPath === requestedPath ? "current" : "";
 
-app.get("/", renderEnglishView);
+const renderView = ({viewName, language}) => {
+  return (request, response) => {
+    let viewVars = {
+      requestedPath: request.path,
+      language: language
+    };
+
+    response.render(viewName, viewVars);
+  }
+};
+
+const englishViewArgs = {viewName: "hello-world-english", language: "en-US"};
+const frenchViewArgs = {viewName: "hello-world-french", language: "fr-FR"};
+const japaneseViewArgs = {viewName: "hello-world-japanese", language: "ja-JP"};
+const serbianViewArgs = {viewName: "hello-world-serbian", language: "sr-Cyrl-rs"};
+
+const renderEnglishView = renderView(englishViewArgs);
+const renderFrenchView = renderView(frenchViewArgs);
+const renderJapaneseView = renderView(japaneseViewArgs);
+const renderSerbianView = renderView(serbianViewArgs);
+
+const redirectToEnglishView = (request, response) => response.redirect("/english");
+
+app.get("/", redirectToEnglishView);
 app.get("/english", renderEnglishView);
 app.get("/french", renderFrenchView);
 app.get("/serbian", renderSerbianView);
